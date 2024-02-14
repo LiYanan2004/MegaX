dc851de47e8e0556f42a744a87e0226658ae8817
LiYanan2004 <37542129+LiYanan2004@users.noreply.github.com>
2024-02-14 17:15:02 +0800

    Add docs

--- Sources/MegaX/Environment/DeviceType.swift
+++ Sources/MegaX/Environment/DeviceType.swift
@@ -1,28 +1,39 @@
 import SwiftUI
 
+/// Device type enum that represent the device users are currently using.
 public enum DeviceType: Int, @unchecked Sendable {
+    /// Devices running iOS, typically iPhone and iPod.
     case phone
+    /// Devices running iPadOS.
     case pad
+    /// Devices running tvOS.
     case tv
+    /// Devices running macOS and fully native macOS experience.
     case mac
+    /// Devices running macOS but the app experience is powered by Mac Catalyst.
+    case macCatalyst
+    /// Devices running visionOS.
     case vision
+    /// Devices in CarPlay experience.
     case carPlay
+    /// Unknown type of current device.
     case unspecified
     
-    init(userInterfaceIdom: UIUserInterfaceIdiom) {
+    internal init(userInterfaceIdom: UIUserInterfaceIdiom) {
         switch userInterfaceIdom {
         case .unspecified: self = .unspecified
         case .phone: self = .phone
         case .pad: self = .pad
         case .tv: self = .tv
         case .carPlay: self = .carPlay
-        case .mac: self = .mac
+        case .mac: self = .macCatalyst
         case .vision: self = .vision
         @unknown default: self = .unspecified
         }
     }
 }
 
+/// Environment key for accessing device type.
 struct DeviceTypeEnvironmentKey: EnvironmentKey {
     #if os(macOS)
     static let defaultValue: DeviceType = .mac
@@ -36,6 +47,10 @@ struct DeviceTypeEnvironmentKey: EnvironmentKey {
 }
 
 extension EnvironmentValues {
+    /// The type of current device.
+    ///
+    /// The app natively to the macOS or using Catalyst typically using different layout strategy,
+    /// so `DeviceType.mac` and `DeviceType.macCatalyst` are two different targets.
     public var deviceType: DeviceType {
         get { self[DeviceTypeEnvironmentKey.self] }
     }
--- Sources/MegaX/Modifiers/if+View.swift
+++ Sources/MegaX/Modifiers/if+View.swift
@@ -2,7 +2,44 @@ import SwiftUI
 
 extension View {
     @ViewBuilder
-    public func `if`(_ condition: @autoclosure () -> Bool, modified: (Self) -> some View) -> some View {
+    /// Adds conditional modifiers to your view.
+    /// - parameters:
+    ///     - condition: Determines whether to apply the modifications.
+    ///     - modified: Modified version of the view if appropriate.
+    ///     - elseModified: Modified version of the view when the condition returns false.
+    /// - returns: A modified view based on the condition.
+    ///
+    /// Avoid using too complex condition rules that will be changed too often,
+    /// because this will lead to unexpected behavior and poor performance.
+    /// Use this modifier when there are platform specific adjustments, for example,
+    /// adding paddings just for iPad environent.
+    public func `if`(
+        _ condition: @autoclosure () -> Bool,
+        modified: (Self) -> some View,
+        else elseModified: (Self) -> some View
+    ) -> some View {
+        if condition() {
+            modified(self)
+        } else {
+            elseModified(self)
+        }
+    }
+    
+    @ViewBuilder
+    /// Adds conditional modifiers to your view.
+    /// - parameters:
+    ///     - condition: Determines whether to apply the modifications.
+    ///     - modified: Modified version of the view if appropriate.
+    /// - returns: A modified or unmodified view based on the condition.
+    ///
+    /// Avoid using too complex condition rules that will be changed too often,
+    /// because this will lead to unexpected behavior and poor performance.
+    /// Use this modifier when there are platform specific adjustments, for example,
+    /// adding paddings just for iPad environent.
+    public func `if`(
+        _ condition: @autoclosure () -> Bool,
+        modified: (Self) -> some View
+    ) -> some View {
         if condition() {
             modified(self)
         } else {
