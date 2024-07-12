4474b4b458cdcf75c4bf2bd7aeb874855e0afe2d
LiYanan2004 <37542129+LiYanan2004@users.noreply.github.com>
2024-07-12 23:53:46 +0800

    Add more customization APIs

--- Sources/MegaX/Camera/Model/CameraCaptureConfiguration.swift
+++ Sources/MegaX/Camera/Model/CameraCaptureConfiguration.swift
@@ -7,11 +7,11 @@ struct CameraCaptureConfiguration: Sendable, Equatable {
     /// Enable automatic lens switching if the device has multiple constituent devices.
     var autoSwitchingLens = true
     /// Enable zero shutter lag if the device supported.
-    var preferZeroShutterLag = true
+    var preferZeroShutterLag = false
     /// Enable responsive capture if the device supported.
-    var preferResponsiveCapture = true
+    var preferResponsiveCapture = false
     /// Enable fast capture prioritization if the device supported.
-    var preferFastCapturePrioritization = true
+    var preferFastCapturePrioritization = false
     /// Enable auto deferred photo delivery if the device supported.
     var preferAutoDeferredPhotoDelivery = false
     #if os(iOS) || os(tvOS)
@@ -22,4 +22,9 @@ struct CameraCaptureConfiguration: Sendable, Equatable {
     var preferConstantColor = false
     /// A Boolean value that indicates whether to deliver a fallback photo when taking a constant color capture without enough confidence.
     var enableConstantColorFallbackDelivery = false
+    /// Prefered capture device types.
+    /// - note: By default, it uses composed device type or wide-angle camera.
+    var captureDeviceTypes: [AVCaptureDevice.DeviceType]? = nil
+    /// Determine how photo pipeline adjust the photo (reduce noise, sharper details and more.)
+    var preferedQualityPrioritization = AVCapturePhotoOutput.QualityPrioritization.balanced
 }
--- Sources/MegaX/Camera/Modifiers.swift
+++ Sources/MegaX/Camera/Modifiers.swift
@@ -4,6 +4,34 @@ import AVFoundation
 @available(watchOS, unavailable)
 @available(visionOS, unavailable)
 extension View {
+    /// Sets the device types for the camera session to find the appropriate capture device.
+    ///
+    /// On iOS, it uses a virtual device which consists of all physical lenses.
+    ///
+    /// On macOS, it uses wide-angle camera and continuity cameras.
+    ///
+    /// If you want to use a specific lens, you can use this modifier to configure the right lens.
+    ///
+    /// - note: Be sure to contain `.builtInWideAngleCamera` as a fallback camera.
+    @available(iOS 17.0, tvOS 17.0, macOS 14.0, *)
+    public func captureDeviceTypes(_ deviceTypes: AVCaptureDevice.DeviceType...) -> some View {
+        transformEnvironment(\._captureConfiguration) { configuration in
+            configuration.captureDeviceTypes = deviceTypes
+        }
+    }
+    
+    /// Set quality prioritization for output photos.
+    ///
+    /// Default value is `.balanced`.
+    ///
+    /// The better quality prioritization means it takes more time to process the photo.
+    @available(iOS 17.0, tvOS 17.0, macOS 14.0, *)
+    public func captureQualityPrioritization(_ prioritization: AVCapturePhotoOutput.QualityPrioritization) -> some View {
+        transformEnvironment(\._captureConfiguration) { configuration in
+            configuration.preferedQualityPrioritization = prioritization
+        }
+    }
+    
     /// Enable auto deferred photo delivery if the device supports.
     @available(macOS, unavailable)
     @available(tvOS, unavailable)
@@ -23,21 +51,18 @@ extension View {
     }
     
     /// Enable responsive capture if the device supports.
+    ///
+    /// Zero shutter lag will be automatically enabled if you enable responsive capture.
+    ///
+    /// - parameter fastCapturePrioritized: A Boolean value that indicates whether the output enables fast capture prioritization.
     @available(iOS 17.0, tvOS 17.0, macOS 14.0, *)
-    public func responsiveCaptureEnabled(_ enabled: Bool = true) -> some View {
+    public func responsiveCaptureEnabled(_ enabled: Bool = true, fastCapturePrioritized: Bool = true) -> some View {
         transformEnvironment(\._captureConfiguration) { configuration in
             configuration.preferResponsiveCapture = enabled
-        }
-    }
-    
-    /// Enable fast capture prioritization if the device supports.
-    @available(iOS 17.0, tvOS 17.0, macOS 14.0, *)
-    public func fastCapturePrioritizationEnabled(_ enabled: Bool = true) -> some View {
-        transformEnvironment(\._captureConfiguration) { configuration in
             if enabled {
-                configuration.preferResponsiveCapture = true
+                configuration.preferZeroShutterLag = true
+                configuration.preferFastCapturePrioritization = fastCapturePrioritized
             }
-            configuration.preferFastCapturePrioritization = enabled
         }
     }
 
@@ -69,6 +94,8 @@ extension View {
     }
     
     /// Prefer capture content in constant color if the device is capable.
+    ///
+    /// - note: If flash mode is off, constant color will be disabled even if the device is capable.
     @available(iOS 18.0, tvOS 18.0, macOS 15.0, *)
     public func cameraConstantColorEnabled(_ enabled: Bool = true, fallbackDeliveryEnabled: Bool = true) -> some View {
         transformEnvironment(\._captureConfiguration) { configuration in
